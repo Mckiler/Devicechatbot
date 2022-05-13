db88175a58ecdbf522550d144ad71d20a42e74d4
Mckiler <mcmckiler@gmail.com>
2022-05-13 20:10:46 +0700

    ChatbotDevice
    ChatbotDeviceV0.2

---
+++ ChatbotDeviceV0.2_medium.ino.ino
@@ -0,0 +1,90 @@
+// ESP32 Node32S
+
+#include <SPI.h>
+#include <Wire.h>
+
+// Wifi Library
+#include <ETH.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+
+///////////////////////////////////////////////////////////////////////////
+// Set LED Pin
+int LED1 = 13;
+String command; 
+
+const char* ssid = "HoneyMcHome_AP";
+const char* password = "honeymc3133";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 500;
+unsigned long currentTime = millis();
+unsigned long previousTime = 0; 
+const long timeoutTime = 2000;
+
+WiFiServer server(80);
+int output5State = 0;
+
+String header;
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) {                             // If a new client connects
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+       // Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          if (currentLine.length() == 0) {
+            if (header.indexOf("GET /on") >= 0) {
+              digitalWrite(LED1, HIGH);
+              Serial.println("เปิดไฟ");
+              
+            } else if (header.indexOf("GET /off") >= 0) {
+              digitalWrite(LED1, LOW);
+              Serial.println("ปิดไฟ");
+            } 
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    header = "";
+    client.stop();
+
+  }
+}
